c54cb4a4ef9443eee225e7dadec0fef53ddb96fe
N.Y.Q <1035415060@qq.com>
2026-02-28 10:27:24 +0800

    Massively slim repository artifacts and broken package entrypoints

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,7 @@
-# from .E_TRFNetv0 import E_TRFNetv0
-from .EVDI import EVDI
-from .REFID.REFID_twosharpInterp import REFID
+"""Model package entrypoint for the simplified branch.
 
-# RGB method
-from .SuperSlomo.runSuperSlomo import SuperSlomo
-from models.Expv8_large.runExpv8_large import Expv8_large
+This snapshot keeps base training wrappers that are present in-tree.
+"""
 
-# For peer comparisons
-from models.timelens.runtimelens import TimeLens
-from models.timelens_flow.runtimelens import TimeLens_flow
-from models.Expv8_Lights3.runExpv8_Lights3 import Expv8_Lights3
-
-from models.CBMNet.runmycbmnet import myCBMNet
-from models.RGBGT.runRGBGT import RGBGT
+from .BaseModel import BaseModel
+from .OursBaseModel import OursBase
