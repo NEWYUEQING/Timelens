062a60bad535c8fff38930e283799a83d2c94b0d
N.Y.Q <1035415060@qq.com>
2026-02-28 10:05:18 +0800

    Adapt BaseLoader for frame-aligned event maps

--- dataset/BaseLoaders/baseloader.py
+++ dataset/BaseLoaders/baseloader.py
@@ -1,9 +1,6 @@
 import torch
-from torch.nn.functional import interpolate
-from torchvision.transforms import ToTensor, ToPILImage
+from torchvision.transforms import ToTensor
 import numpy as np
-import glob
-from natsort import natsorted as sorted
 from torch.utils.data import Dataset
 import os
 from PIL import Image
@@ -22,12 +19,9 @@ class BaseLoader(Dataset):
         self.interp_ratio = para[self.key]['interp_ratio']
         self.sample_group = self.interp_ratio - 1 if 'sample_group' not in para[self.key].keys() else para[self.key]['sample_group']
         self.random_t = para[self.key]['random_t']
-        self.toim = ToPILImage()
         self.totensor = ToTensor()
         self.samples_indexing()
         self.color = 'gray' if 'color' not in para[self.key].keys() else para[self.key]['color']
-        self.events_channel = 128 if 'events_channel' not in para.keys() else para.events_channel
-        # self.events_channel
         if len(self.samples_list) > 0:
             item = 0
             item_content = self.samples_list[item]
@@ -41,12 +35,10 @@ class BaseLoader(Dataset):
             indexes = list(range(0, len(rgb_path),
                                  self.rgb_sampling_ratio))
             for i_ind in range(0, len(indexes) - self.interp_ratio, 1 if self.training_flag else self.interp_ratio):
-                # print(i_ind, self.interp_ratio, len(indexes), indexes[0], indexes[-1], len(rgb_path), len(evs_path))
                 rgb_sample = [rgb_path[sind] for sind in indexes[i_ind:i_ind + self.interp_ratio + 1]]
-                evs_sample = evs_path[indexes[i_ind]:indexes[i_ind + self.interp_ratio]]
+                evs_sample = [evs_path[sind] for sind in indexes[i_ind:i_ind + self.interp_ratio + 1]]
                 rgb_name = [os.path.splitext(os.path.split(rs)[-1])[0] for rs in rgb_sample]
                 self.samples_list.append([k, rgb_name, rgb_sample, evs_sample])
-        # self.samples_list = [self.samples_list[0]]
         return
 
     def __len__(self):
@@ -60,48 +52,19 @@ class BaseLoader(Dataset):
         return im
 
     def ereader(self, events_path):
-        # evs_data = [self.totensor(np.load(ep, allow_pickle=True)['data']) for ep in events_path]
-        single_event_channel = self.events_channel / len(events_path)
         evs_data = []
         for epath in events_path:
             edata = np.load(epath, allow_pickle=True)['data']
-            h, w = edata.shape[-2:]
             if len(edata.shape) == 2:
                 edata = np.expand_dims(edata, 0)
-            c = edata.shape[0]
-
-            if c <single_event_channel:
-                edata = np.concatenate((edata, np.zeros((int(single_event_channel)-c, h, w), dtype=np.float32)), 0)
             evs_data.append(torch.from_numpy(edata))
-
-        evs_data = torch.cat(evs_data, 0).float()
-        if c > single_event_channel:
-            acc_chn = c // single_event_channel
-            evs_data = evs_data.view(int(acc_chn), -1, h, w).sum(0)
-        return evs_data
-
-    def adaptive_wei(self, ts, span_leftB, span_rightB):
-        ## calculate weights, i.e., \omega & 1-\omega in paper
-        ## This func coming from original code
-        right = 0
-        left = 0
-        if (span_leftB[1] < ts) and (ts < span_rightB[0]):
-            right = (ts - span_leftB[0]) / (span_rightB[1] - span_leftB[0])
-            left = 1 - right
-        if ts >= span_rightB[0]:
-            right = 1
-        if ts <= span_leftB[1]:
-            left = 1
-        sum_value = right + left
-        right = right / sum_value
-        left = left / sum_value
-        return left, right
+        return torch.cat(evs_data, 0).float()
 
     def data_loading(self, paths, sample_t):
         folder_name, rgb_name, rgb_sample, evs_sample = paths
         im0 = self.imreader(rgb_sample[0])
         im1 = self.imreader(rgb_sample[-1])
-        events = self.ereader(evs_sample)
+        events = self.ereader([evs_sample[st] for st in sample_t])
         gts = [self.imreader(rgb_sample[st]) for st in sample_t]
         return folder_name, rgb_name, im0, im1, events, gts
 
