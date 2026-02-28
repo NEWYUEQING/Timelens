7f09174675aaa3400483c9a799a8526a348b9d16
N.Y.Q <1035415060@qq.com>
2026-02-28 14:37:22 +0800

    Collapse model package into a single file for simpler browsing

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,1 @@
-"""Model package entrypoint for the simplified branch.
-
-This snapshot keeps base training wrappers that are present in-tree.
-"""
-
-from .BaseModel import BaseModel
-from .OursBaseModel import OursBase
+from .model import BaseModel, OursBase, ForwardWarp
